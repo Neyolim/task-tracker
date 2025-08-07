4a2538a56203bf0a6b4954c66362e9a04e672253
Neyolim <neyoollim@gmail.com>
2025-08-07 20:59:05 +0545

    add reducers

--- src/features/ToDo/todoSlice.js
+++ src/features/ToDo/todoSlice.js
@@ -1,11 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos:[{
-        id:"abc",
-        task:"demo-task",
-        isDone:false
-    }],
-}
+  todos: [
+    {
+      id: "abc",
+      task: "demo-task",
+      isDone: false,
+    },
+  ],
+};
 
-export const todoSlice = createSlice({})
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: nanoid(),
+        task: action.payload,
+        isDone: false,
+      };
+      state.todos.push(newTodo); //direct mutation is possible with redux
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id != action.payload);
+    },
+    markAsDone: (state, action) => {
+      state.todos = state.todos.map((todo) => {
+        if (todo.id === action.payload) {
+          todo.isDone = true;
+        }
+      });
+    },
+  },
+});
+
+export const { addTodo, deleteTodo, markAsDone} = todoSlice.actions;
+export default todoSlice.reducer;
